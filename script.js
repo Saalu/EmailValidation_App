118b13623a5c966266e7d5d464a771eb030c6a97
unknown <saaluissaka>
2020-09-14 10:02:21 +0000

    localstorage add

--- script.js
+++ script.js
@@ -5,31 +5,32 @@ const submitBtn = document.getElementById('submit'),
         password =document.getElementById('password'),
         password2 =document.getElementById('password-check'),
         resetBtn=document.getElementById('resetBtn'),
-        emailForm =document.getElementById('input-form')
+        emailForm =document.getElementById('input-form'),
+        listcontainer=document.querySelector('.list-items')
 
 // eventListeners
 eventListeners()
 function eventListeners(){
     emailForm.addEventListener('submit' , onSubmit)
     document.addEventListener('DOMContentLoaded', onPageLoad)
+    resetBtn.addEventListener('click', resetForm)
+    listcontainer.addEventListener('click', removeItem)
 }
 
-function onPageLoad(){
 
-    submitBtn.disable = true
 
+function resetForm(){
+
+    emailForm.reset()
 }
-
 // functions 
 function onSubmit (e){
 e.preventDefault()
-
 
     checkInputs()
 }
 
 function checkInputs(){
-
     const usernameValue = username.value.trim()
     const emailValue= email.value.trim()
     const passwordValue = password.value.trim()
@@ -43,7 +44,6 @@ function checkInputs(){
     }else{
         setSuccessFor(username)
     }
-
     // email
     if(emailValue === ''){
         setErrorFor(email, 'Email cannot be blank')
@@ -53,7 +53,6 @@ function checkInputs(){
     else{
         setSuccessFor(email)
     }
-
     // password
     if(passwordValue === ''){
         setErrorFor(password, 'password cannot be blank')
@@ -71,19 +70,11 @@ function checkInputs(){
     else{
         setSuccessFor(password2)
     }
-    
-
 
     function isEmail(email){
         return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
 
     }
-
-
-
-
-
-
     // success and error function
     function setErrorFor(input, message){
         const inputControl =input.parentElement  //.inputBox
@@ -99,8 +90,85 @@ function checkInputs(){
         inputControl.className='inputBox success'
     }
 
+    // 
+
+    listTemplate( emailValue)
+
+    addToStorage(emailValue)
+}
+/* ===Input Validation Ended====  */
+function removeItem(e){
+
+    if(e.target.classList.contains('list-items')){
+        console.log('yes')
+    }
+}
 
 
+/* === Saving to List Functions ====  */
 
+// const table = document.querySelector('table')
+function  listTemplate(email){
+    // const listcontainer = document.querySelector('.list-items')
+    const row = document.createElement('tr')
+    row.innerHTML =`
+     <td >${email}</td>
+     <td><i class="fas fa-check update"></i> <i class="fas fa-trash delete"></i></td>
+
+    `;
+
+    listcontainer.appendChild(row)
+}
+// storage func
+function addToStorage(email ){
+    let storeItems =getFromStorage()
+
+    storeItems.push(email)
+
+    localStorage.setItem('items', JSON.stringify(storeItems))
+
+    console.log(storeItems)
+}
+
+function getFromStorage(){
+    let storeItems = localStorage.getItem('items')
+    let items
+    if(storeItems === null){
+        items = []
+    }else{
+        items = JSON.parse(storeItems)
+    }
+    return items
+
+}
+
+function removeItem(e){
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.parentElement.remove()
+    }
+
+    deleteFromStorage(e.target.parentElement.previousElementSibling)
+}
+
+function deleteFromStorage(item){
+  let storeItems = getFromStorage()
+  
+            storeItems.forEach((itemLS, index)=> {
+            if(itemLS === item.textContent){
+                console.log('yes true')
+                storeItems.splice(index, 1)
+            }
+        })
+        localStorage.setItem('items', JSON.stringify(storeItems))
+}
+
+
+function onPageLoad(){
+    let storeItems =getFromStorage()
+
+    storeItems.forEach( (item)=>{
+        listTemplate(item)
+        
+    })
 
 }
